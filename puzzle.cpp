0ccb6a6bc3bd3bbd4130f52c62251ced3a37334f
Vlad Isan <vlad@innerbyte.uk>
2023-12-05 20:45:31 +0000

    tidying up

--- puzzle.cpp
+++ puzzle.cpp
@@ -17,22 +17,7 @@ int puzzle_sample_1(std::string base_file_path) {
         return 0;
     }
 
-    std::string line;
-    uint32_t value = 0;
-
-    while (std::getline(file, line)) {
-        auto first_digit = std::ranges::find_if(line, [](char c) { return std::isdigit(c); });
-        auto last_digit = std::ranges::find_if(line | std::views::reverse, [](char c) { return std::isdigit(c); });
-
-        if (first_digit != line.end() && last_digit != line.rend()) {
-            auto number_string = fmt::format("{}{}", *first_digit, *last_digit);
-            value += std::stoi(number_string);
-        }
-    }
-
-    fmt::println("Total is {}", value);
-
-    return value;
+    return do_puzzle_1(file);
 }
 
 int puzzle_sample_2(std::string base_file_path) {
@@ -48,6 +33,42 @@ int puzzle_sample_2(std::string base_file_path) {
         return 0;
     }
 
+    return do_puzzle_2(file);
+}
+
+int puzzle_1(std::string base_file_path) {
+    std::string file_path = fmt::format("{}/{}", base_file_path, "puzzle-input-1.txt");
+
+    std::ifstream file(file_path);
+
+    if (!file.is_open()) {
+        fmt::println("{}", file_path);
+        fmt::println("Something happened while opening the file. EAT DEEZNUTS!!!");
+        fmt::println("Error {}", strerror(errno));
+
+        return 0;
+    }
+
+    return do_puzzle_1(file);
+}
+
+int puzzle_2(std::string base_file_path) {
+    std::string file_path = fmt::format("{}/{}", base_file_path, "puzzle-input-1.txt");
+
+    std::ifstream file(file_path);
+
+    if (!file.is_open()) {
+        fmt::println("{}", file_path);
+        fmt::println("Something happened while opening the file. EAT DEEZNUTS!!!");
+        fmt::println("Error {}", strerror(errno));
+
+        return 0;
+    }
+
+    return do_puzzle_2(file);
+}
+
+int do_puzzle_1(std::ifstream&file) {
     std::string line;
     uint32_t value = 0;
 
@@ -66,19 +87,7 @@ int puzzle_sample_2(std::string base_file_path) {
     return value;
 }
 
-int puzzle_1(std::string base_file_path) {
-    std::string file_path = fmt::format("{}/{}", base_file_path, "puzzle-input-1.txt");
-
-    std::ifstream file(file_path);
-
-    if (!file.is_open()) {
-        fmt::println("{}", file_path);
-        fmt::println("Something happened while opening the file. EAT DEEZNUTS!!!");
-        fmt::println("Error {}", strerror(errno));
-
-        return 0;
-    }
-
+int do_puzzle_2(std::ifstream&file) {
     std::string line;
     uint32_t value = 0;
 
