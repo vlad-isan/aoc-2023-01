11becc50cf76f72489072cab852bd71ba7f043f0
Vlad Isan <vlad@innerbyte.uk>
2023-12-05 20:37:35 +0000

    day 1 - puzzle 1

---
+++ puzzle.h
@@ -0,0 +1,21 @@
+//
+// Created by Vlad Isan on 05/12/2023.
+//
+
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
+#include <algorithm>
+#include <cctype>
+#include <fstream>
+#include <iostream>
+#include <ranges>
+#include <string>
+#include "fmt/core.h"
+
+int puzzle_sample_1(std::string base_file_path);
+int puzzle_sample_2(std::string base_file_path);
+int puzzle_1(std::string base_file_path);
+int puzzle_2(std::string base_file_path);
+
+#endif //PUZZLE_H
